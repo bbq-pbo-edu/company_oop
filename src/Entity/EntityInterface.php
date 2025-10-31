ce570e401b4152155888fa7140ae489bef6f3aa8
bbq-po-edu <231660485+bbq-pbo-edu@users.noreply.github.com>
2025-10-31 09:57:01 +0100

    feat: add Employee class skeleton

--- src/Entity/EntityInterface.php
+++ src/Entity/EntityInterface.php
@@ -6,3 +6,58 @@ interface EntityInterface
 {
 
 }
+
+class Employee
+{
+    private int $id;
+    private string $firstName;
+    private string $lastName;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+}
